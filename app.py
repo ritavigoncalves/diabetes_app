345fccf425944f815fcdb28799c8116e1fd58f43
Rita Goncalves <ritavieiragoncalves@gmail.com>
2020-06-26 14:35:04 +0100

    New Files

--- app.py
+++ app.py
@@ -3,14 +3,38 @@ import pandas as pd
 from flask import Flask, request, jsonify, render_template, json, Response, send_file
 from models import find_item
 from fuzzywuzzy import process
+import dataikuapi
+
 
 app = Flask(__name__)
 
+# ------------------------- API ------------------------- # 
+host = "https://wcsc1.dss-demo.dataiku.com/"
+apiKey = "XCF82CIESH57BCLER0LG74YVIU8GLGSI"
+client = dataikuapi.DSSClient(host, apiKey)
 
-data = pd.read_csv('static/python1_groomed.csv') 
-df1 = pd.DataFrame(data=data)
+client.list_project_keys()
 
-# ['product_name', 'fat_100g', 'carbohydrates_100g', 'proteins_100g', 'carb_classifier', 'insulin_intake']
+project = client.get_project("DATAFORCE")
+dataset = project.get_dataset("python1_groomed") 
+
+# ------------------------- API DATAFRAME ------------------------- # 
+columns = [column['name'] for column in dataset.get_schema()['columns']]
+row_count = 0
+data = [ ]
+
+for row in dataset.iter_rows():
+    data.append(row)
+    row_count = row_count + 1
+    if row_count >= 10000:
+        break
+
+df1 = pd.DataFrame(data=data, columns=columns)
+
+# ------------------------- CSV ------------------------- # 
+# data = pd.read_csv('static/python1_groomed.csv') 
+# df1 = pd.DataFrame(data=data)
+
 
 # ------------------------- ROUTES ------------------------- # 
 @app.route('/')
